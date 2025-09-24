0a9acfe14c3ad4321a103bb435d7569b78ebe32c
MadalynL <madalynmlindberg@gmail.com>
2025-09-24 13:48:41 -0400

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -2,6 +2,13 @@ def evaluate_polynomial(degree, x, constant_term, *coefficients):
     # TODO: Implement polynomial evaluation using direct substitution method
     # TODO: Print step-by-step evaluation (S0, S1, S2, etc.)
     # TODO: Return final polynomial result
+
+    p=constant_term
+    k=1
+    while k<= degree:
+        p= p + x**k * coefficients
+        k= k + 1
+    return p
     pass
 
 if __name__ == "__main__":
@@ -9,4 +16,6 @@ if __name__ == "__main__":
     # TODO: Get user input for degree, x, constant term, and coefficients
     # TODO: Call evaluate_polynomial function
     # TODO: Ask user if they want to run again
+
+
     pass
