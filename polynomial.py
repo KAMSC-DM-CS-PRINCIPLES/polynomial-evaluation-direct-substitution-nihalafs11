1f91728cd5b26082606f332221c1a25821ab05b4
Robbiesactforthehub <03robert.strongmorse@kamsc.org>
2025-09-24 13:54:14 -0400

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -4,7 +4,7 @@ def evaluate_polynomial(degree, x, constant_term, *coefficients):
     pwr=x
     if (degree!=len(coefficients)):
         return "Should have raised ValueError"
-    while (k<degre+1):
+    while (k<degree+1):
         s+=coefficients[k-1]*pwr**k
         k+=1
     return s
