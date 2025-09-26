6c8956e75c7d0823a65797b0ea39cff28d914b6c
TejashreyaD <tejashreyad@gmail.com>
2025-09-25 20:19:28 -0400

    yes

--- polynomial.py
+++ polynomial.py
@@ -1,12 +1,7 @@
 def evaluate_polynomial(degree, x, constant_term, *coefficients):
-    # TODO: Implement polynomial evaluation using direct substitution method
-    # TODO: Print step-by-step evaluation (S0, S1, S2, etc.)
-    # TODO: Return final polynomial result
-    pass
-
-if __name__ == "__main__":
-    # TODO: Add main program loop
-    # TODO: Get user input for degree, x, constant term, and coefficients
-    # TODO: Call evaluate_polynomial function
-    # TODO: Ask user if they want to run again
-    pass
+    result = constant_term
+    i = len(coefficients) - 1
+    while i >= 0:
+        result *= x**i + coefficients[i]
+        i -= 1
+    return result
