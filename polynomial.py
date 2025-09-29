11439ae74ab23869f92c5dcf7fc13f43478de0cd
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-28 22:41:04 -0400

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -1,13 +1,13 @@
 def evaluate_polynomial(degree, x, constant_term, *coefficients):
     # set P and k
-    k = 1
+    k = 0
     P = constant_term
     print(f"S0 (value of the constant term) = {P}")
-    while k <= degree:
-        P = P + str(coefficients[k] * (x ** (k)))
+    while k < degree-1:
+        P = str(int(P) + coefficients[k] * (x ** (k)))
+        print(f"S{k+1} (Sum of the {k} lowest terms) = {P}")
         k = k + 1
-        print(f"S{k} (value of the polynomial) = {P}")
-    print(f"P(x)={P}")
+    print(f"P(x)= {P}")
 
     # TODO: Implement polynomial evaluation using direct substitution method
     # TODO: Print step-by-step evaluation (S0, S1, S2, etc.)
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     degree = int(input("Degree of the polynomial: "))
     x = int(input("Value of x: "))
     constant_term = int(input("Value of constant term: "))
-    for i in range(degree):
-        coefficients.append((input(f"Value of coefficient x^{str(i+1)}: ")))
+    for i in range(degree-1):
+        coefficients.append(int(input(f"Value of coefficient x^{str(i+1)}: ")))
     evaluate_polynomial(degree, x, constant_term, *coefficients)
 
 
