378745048d10cd676fd96645cb7f45d53f8c17e2
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-30 10:54:44 -0400

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -4,7 +4,7 @@ def evaluate_polynomial(degree, x, constant_term, *coefficients):
     P = constant_term
     print(f"S0 (value of the constant term) = {P}")
     if len(coefficients) != degree:
-        raise ValueError
+        raise ValueError(f"need {degree} coefficient(s)")
     while k <= degree:
         P = str(int(P) + coefficients[k-1] * (x ** (k)))
         print(f"S{k} (Sum of the {k+1} lowest terms) = {P}")
@@ -18,13 +18,17 @@ def evaluate_polynomial(degree, x, constant_term, *coefficients):
 
 
 if __name__ == "__main__":
-    coefficients = ()
-    degree = int(input("Degree of the polynomial: "))
-    x = int(input("Value of x: "))
-    constant_term = int(input("Value of constant term: "))
-    for i in range(degree):
-        coefficients+=(int(input("Value of coefficient: ")), )
-    evaluate_polynomial(degree, x, constant_term, *coefficients)
+    while True:
+        coefficients = ()
+        degree = int(input("Degree of the polynomial: "))
+        x = int(input("Value of x: "))
+        constant_term = int(input("Value of constant term: "))
+        for i in range(degree):
+            coefficients+=(int(input("Value of coefficient: ")), )
+        evaluate_polynomial(degree, x, constant_term, *coefficients)
+        again = input("Run again? (y/n): ")
+        if again != "y":
+            break
 
 
 
